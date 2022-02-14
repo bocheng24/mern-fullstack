775fec0f010d980d7f7790bde1629b8b1e323d76
bocheng24 <jeffcgp@hotmail.com>
2022-02-14 22:38:14 +0800

    express init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Create .env in the root folder, and write the following content
+### NODE_ENV = development
+### PORT = <port_number>
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,6 @@
-console.log('server side')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 4500
+
+const app = express()
+app.listen(PORT, () => console.log('server side is running:', PORT))
