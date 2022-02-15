2848e114336a4032406e03d3c7401da99a722e96
bocheng24 <jeffcgp@hotmail.com>
2022-02-15 18:11:50 +0800

    implement put, delete api with mongoose

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -24,15 +24,31 @@ const postGoal = asyncHandler(async (req, res) => {
 })
 
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: `update goal: ${req.params.id}`
-    })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedGoal)
 })
 
 const delGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: `Delete goal: ${req.params.id}`
-    })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
