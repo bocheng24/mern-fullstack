3969c977d9b5337a269633f876786fa1d66bc518
bocheng24 <jeffcgp@hotmail.com>
2022-02-16 22:33:13 +0800

    Add full user creation logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,18 +5,50 @@ const User = require('../models/userModel')
 
 const signupUser = asyncHandler(async (req, res) => {
     
-    if (!req.body.username) {
+    const { username, email, password } = req.body
+
+    // check if all fields are fulfilled
+    if (!username || !email || !password) {
+
+        const usernameErr = username ? '' : 'username '
+        const emailErr = email ? '' : 'email '
+        const passwordErr = password ? '' : 'password '
+
         res.status(400)
-        throw new Error('Username is empty')
+        throw new Error(`Please fill ${usernameErr}${emailErr}${passwordErr}field(s)`)
     }
 
+    // check if the user has been signed up
+    const usernameExists = await User.findOne({ username })
+    const emailExists = await User.findOne({ email })
+
+    if (usernameExists || emailExists) {
+        const message = `${ usernameExists ? 'username ' : '' }${ emailExists ? 'email ': '' }already signed up`
+        res.status(400)
+        throw new Error(message)
+    }
+
+    // Gen hash password and add user
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(password, salt)
+
     const user = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+        username: username,
+        email: email,
+        password: hashPassword
     })
 
-    res.status(200).json(user)
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            username: user.username,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
 
 })
 
