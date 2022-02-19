3beb6d8d33da4134a8f155e93d0dc370bf549915
bocheng24 <jeffcgp@hotmail.com>
2022-02-19 23:42:24 +0800

    react router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { 
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import Dashboard from './components/dashboard/dashboard'
+import Signin from './components/authentication/signin'
+import Signup from './components/authentication/signup'
 
 function App() {
   return (
-    <div>
-      <h1>Mern frontend</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Dashboard /> } />
+        <Route path='/signin' element={ <Signin /> } />
+        <Route path='/signup' element={ <Signup /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
